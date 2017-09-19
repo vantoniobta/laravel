a27f350c7c7b67bce01f8f287fa8dca3f95b55a1
valentin antonio <vale.residencia@gmail.com>
2017-09-18 19:37:11 -0500

    view

--- routes/web.php
+++ routes/web.php
@@ -10,30 +10,53 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+use App\Http\Middleware\CheckAge;
+use Illuminate\Support\Facades\View;	
+
+
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/foo', function(){
-	return redirect('redirect');
-});
-
-Route::get('/showRedirect', function(){
-	return '<h1>Hi redirect route here</h1>';
-})->name('redirect');
-
-Route::view('/test', 'test');
-
-
-Route::get('user/{id}', function($id){
-	return '<h1>User'.$id."</h1>";
+Route::get('/home', function(){
+	return '<h1>Your age is less then 100 or equal 100</h1>';
 });
 
 
-Route::get('user/{name?}', function ($name = null) {
-    return $name;
+Route::get('/user/age/{age}', function(){
+	return 'Your have permmision';
+})->middleware(CheckAge::class);
+
+Route::get('/greeting', function () {
+	if (View::exists('greeting')) {
+		$name = 'valentin';
+       return view('greeting', compact('name'));
+   }else{
+   	return '<h1>no view created</h1>';
+   }
 });
 
-Route::get('user/{name?}', function ($name = 'vantonio') {
-    return $name;
-});
+
+// Route::get('/foo', function(){
+// 	return redirect('redirect');
+// });
+
+// Route::get('/showRedirect', function(){
+// 	return '<h1>Hi redirect route here</h1>';
+// })->name('redirect');
+
+// Route::view('/test', 'test');
+
+
+// Route::get('user/{id}', function($id){
+// 	return '<h1>User'.$id."</h1>";
+// });
+
+
+// Route::get('user/{name?}', function ($name = null) {
+//     return $name;
+// });
+
+// Route::get('user/{name?}', function ($name = 'vantonio') {
+//     return $name;
+// });
