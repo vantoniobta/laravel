5fb33987445b0ad1585b23fee0f8fa05080d5c8e
valentin antonio <vale.residencia@gmail.com>
2017-11-16 18:56:06 -0600

    dashboard nav

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -1,12 +1,11 @@
-@extends('layouts.app')
+@extends('layouts.admin')
 
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
+        <div class="col-md-12">
             <div class="panel panel-default">
                 <div class="panel-heading">ADMIN Dashboard</div>
-
                 <div class="panel-body">
                     @if (session('status'))
                         <div class="alert alert-success">
