52e87e54b9ab1d8ce763ccbf92a79a54a7d1eaa8
vantonio <vale.residencia@gmail.com>
2019-12-02 13:36:06 -0600

    http-foundation update

--- resources/views/mty.blade.php
+++ resources/views/mty.blade.php
@@ -6,33 +6,23 @@
 
 <!-- Slider -->
 
-
-
-
-<!--   <div class="container_title_view">
-       ÚLTIMAS OFERTAS DE TRABAJO MONTERREY , N.L
-  </div> -->
-
   <div class="page_view_section">
 
+
     @forelse ($articles as $work)
-    <a href="{{ URL::to('/articulo', $work->url)}}" style="color:#4d665f">
-      <div class="item_page_view_section">
-        <div>
-          <img src="../../uploads/img_art/{{ $work->image_article }}" width="300" height="200px">
+    
+
+
+    <div class="item_page_view_section">
+      <a href="{{ URL::to('/articulo', $work->url)}}" style="color:#4d665f;">
+      <div>
+        <img src="../../uploads/img_art/{{ $work->image_article }}" width="300" height="200px" class="img_section">
+      </div>
+
+        <div style="background-color: yellow; ">
+          <p style=" font-size: 20px; font-family: 'Lalezar', cursive;color: #333232">{{ $work->title }}</p>
         </div>
-       <div>
-        <div style="background-color: yellow; display: flex; ">
-          <p style=" font-size: 24px; font-family: 'Lalezar', cursive;color: #333232">{{ $work->title }}</p>
-        </div>
-        
-    </a>
-  </div>
-      
-       
-      <!-- <p style=" font-size: 12px"><span style="color: #696969;">Imagen: vantonio</span></p> -->
-      <!-- <p style=" font-size: 14px;color: #000;"><a href="{{ URL::to('/articulo', $work->url)}}" style="color:#000">{{ $work->abilities }}</a></p> -->
-      <!-- <h6 data-fontsize="10" data-lineheight="23"><span style="color: #696969;"><i class="fas fa-map-marker-alt"></i>&nbsp;{{ $work->address }} | {{ date('F d, Y', strtotime($work->created_at)) }}</span></h6> -->
+      </a>
     </div>
 
 
@@ -40,7 +30,7 @@
            <div class="container" id="div_vacantes">
               <div class="row">
                     <div class="col-md-12">
-                               <h3 align="center">No tenemos Vacantes</h3>
+                               <h3 align="center">No tenemos notas</h3>
                     </div>
                 </div>
             </div>
@@ -71,8 +61,8 @@
     <footer class="footer-distributed" >
       <div class="footer-right">
         <p class="footer-company-about">
-          <span>Nosotros</span>
-          Somos una empresa lider en cada uno de los negocios, estamos comprometidos con la mejora continua para satisfacer a nuestros clientes.
+          <span>Huasteco Digital</span>
+          Somos una empresa informativa desde la huasteca para todo México.
         </p>
         <div class="footer-icons">
           <a href="#"><i class="fa fa-facebook"></i></a>
